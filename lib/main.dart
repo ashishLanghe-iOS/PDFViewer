59a5a5be99c394ee3c43c1b11cc28d17fec55a87
Ashish Langhe <langheashish389@gmail.com>
2020-07-27 18:17:54 +0530

    Latest Update

---
+++ lib/main.dart
@@ -0,0 +1,173 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_document_picker/flutter_document_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:http/http.dart' as http;
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String assetPDFPath = "";
+  String urlPDFPath = "";
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter PDF Tutorial"),
+        ),
+        body: Builder(
+          builder: (context) => Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    color: Colors.cyan,
+                    child: Icon(Icons.add_box,
+                    color: Colors.white,
+                    ),
+                    onPressed: () async{
+                      //With parameters:
+                      FlutterDocumentPickerParams params = FlutterDocumentPickerParams(
+                        allowedFileExtensions: ['pdf'],
+                        allowedMimeTypes: ['application/*'],
+                        invalidFileNameSymbols: ['/'],
+                      );
+
+                      final pathk = await FlutterDocumentPicker.openDocument(params: params);
+                      print("SELECTED File Path => $assetPDFPath");
+                      if (pathk != null) {
+                          setState(() {
+                            assetPDFPath = pathk;
+                            print(assetPDFPath);
+                        });
+
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    PdfViewPage(path: assetPDFPath)));
+                      }
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(160, 10, 0, 0),
+                    child: Row(
+                      children: <Widget>[
+                        Text("Select PDF file!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+        ),
+      ),
+    );
+  }
+}
+
+class PdfViewPage extends StatefulWidget {
+  final String path;
+
+  const PdfViewPage({Key key, this.path}) : super(key: key);
+  @override
+  _PdfViewPageState createState() => _PdfViewPageState();
+}
+
+class _PdfViewPageState extends State<PdfViewPage> {
+  int _totalPages = 0;
+  int _currentPage = 0;
+  bool pdfReady = false;
+  PDFViewController _pdfViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Document"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          PDFView(
+            filePath: widget.path,
+            autoSpacing: true,
+            enableSwipe: true,
+            pageSnap: true,
+            swipeHorizontal: true,
+            nightMode: false,
+            onError: (e) {
+              print(e);
+            },
+            onRender: (_pages) {
+              setState(() {
+                _totalPages = _pages;
+                pdfReady = true;
+              });
+            },
+            onViewCreated: (PDFViewController vc) {
+              _pdfViewController = vc;
+            },
+            onPageChanged: (int page, int total) {
+              setState(() {});
+            },
+            onPageError: (page, e) {},
+          ),
+          !pdfReady
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Offstage()
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          _currentPage > 0
+              ? FloatingActionButton.extended(
+                  backgroundColor: Colors.red,
+                  label: Text("Go to ${_currentPage - 1}"),
+                  onPressed: () {
+                    _currentPage -= 1;
+                    _pdfViewController.setPage(_currentPage);
+                  },
+                )
+              : Offstage(),
+          _currentPage+1 < _totalPages
+              ? FloatingActionButton.extended(
+                  backgroundColor: Colors.green,
+                  label: Text("Go to ${_currentPage + 1}"),
+                  onPressed: () {
+                    _currentPage += 1;
+                    _pdfViewController.setPage(_currentPage);
+                  },
+                )
+              : Offstage(),
+        ],
+      ),
+    );
+  }
+}
